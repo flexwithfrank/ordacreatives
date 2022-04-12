cf1c0c33a84fb007b183c50472f02aafe6cf8dc5
videovulture <30363299+videovulture@users.noreply.github.com>
2022-04-12 10:09:05 -0700

    updated emmet

--- src/Avatars.js
+++ src/Avatars.js
@@ -1,12 +1,12 @@
 import * as React from 'react';
 import Avatar from '@mui/material/Avatar';
 import Stack from '@mui/material/Stack';
-import frankieAvatar from '/Users/rodriguezmedia/Desktop/react-counter-app/src/images/avatar-1.png'
-import AvatarOne from '/Users/rodriguezmedia/Desktop/react-counter-app/src/images/avatar-1.png';
-import AvatarTwo from '/Users/rodriguezmedia/Desktop/react-counter-app/src/images/avatar-2.png';
-import AvatarThree from '/Users/rodriguezmedia/Desktop/react-counter-app/src/images/avatar-3.png';
-import AvatarFour from '/Users/rodriguezmedia/Desktop/react-counter-app/src/images/avatar-4.png';
-import ordaLogo from '/Users/rodriguezmedia/Desktop/react-counter-app/src/images/orda-logo.png';
+import frankieAvatar from '../src/images/avatar-1.png'
+import AvatarOne from '../src/images/avatar-1.png';
+import AvatarTwo from '../src/images/avatar-2.png';
+import AvatarThree from '../src/images/avatar-3.png';
+import AvatarFour from '../src/images/avatar-4.png';
+import ordaLogo from '../src/images/orda-logo.png';
 import { AvatarGroup } from '@mui/material';
 
 const logoSize = {
