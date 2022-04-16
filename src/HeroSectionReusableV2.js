2a144ec19eaa4d9a967b1e4f422fa079a28c1686
videovulture <30363299+videovulture@users.noreply.github.com>
2022-04-16 11:49:32 -0700

    updates-5

--- src/HeroSectionReusableV2.js
+++ src/HeroSectionReusableV2.js
@@ -25,7 +25,7 @@ export default function HeroSectionReuseableV2({ header, des, img }) {
                 <Box sx={{ maxWidth: 600, 
                 textAlign: 'left', 
                 alignItems: 'center',
-                pb: 3 }}>
+                pb: 4 }}>
                 <p style={{fontSize: "24px"}}>{des}</p>
                 </Box>
                 <ButtonV2 txt="Get Started with Orda" icn={<CelebrationOutlinedIcon />} /> 
